7299b9933e15d21e299d7e2f5b0ad70a91cad51e
Tyler Bunnell <tylerbunnell@gmail.com>
2017-03-25 15:11:58 -0600

    Add test proving panic deadlocks

--- main_test.go
+++ main_test.go
@@ -33,6 +33,44 @@ func TestEverything(t *testing.T) {
 	err = http.Serve(listener, handler)
 }
 
+func TestPanic(t *testing.T) {
+	var err error
+	addr := net.TCPAddr{
+		IP: []byte{127, 0, 0, 1},
+	}
+	listener, err := net.ListenTCP("tcp4", &addr)
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		handler := myHandler{
+			listener: listener,
+		}
+		err = http.Serve(listener, handler)
+	}()
+
+	port := listener.Addr().(*net.TCPAddr).Port
+	Options.Endpoint = fmt.Sprintf("http://127.0.0.1:%d", port)
+	Options.Token = "fake token"
+	Options.CaptureAllGoroutines = true
+	//Options.DebugBacktrace = true
+	Options.ContextLineCount = 2
+	count := 0
+	for i := 0; i < 5; i++ {
+		defer func() {
+			recover()
+			count++
+		}()
+		defer ReportPanic(nil)
+		// fire off a panic. this should happen 5 times
+		panic("boom")
+	}
+	if count != 5 {
+		// really this doesn't do much, since it won't be hit if the code above deadlocks
+		t.Fatal("Expected 5 panics")
+	}
+}
+
 type myHandler struct {
 	listener *net.TCPListener
 }
