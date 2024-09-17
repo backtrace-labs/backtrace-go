7b1b34ac7baa6b80ac4bbdf06e493c825a5c29a7
Andrzej Pragacz <apragacz@o2.pl>
2024-09-17 15:37:45 +0200

    fix(threads): stacktrace parsing (#15)
    * Move parsing of stack to ParseThreadsFromStack
    
    * Fix stacktrace by removing "in goroutine" from function name
    
    * Remove unnecessary error handling
    
    * Remove unnecessary error var declaration
    
    * Sort imports, remove underscores from package references

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package bt
 
-import "bytes"
-import "net/url"
-import "net/http"
-import crypto_rand "crypto/rand"
-import "encoding/json"
-import "fmt"
-import "io/ioutil"
-import math_rand "math/rand"
-import "os"
-import "regexp"
-import "runtime"
-import "strconv"
-import "strings"
-import "time"
+import (
+	"bytes"
+	cryptorand "crypto/rand"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	mathrand "math/rand"
+	"net/http"
+	"net/url"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+)
 
 const VersionMajor = 0
 const VersionMinor = 0
@@ -36,10 +36,7 @@ type OptionsStruct struct {
 
 var Options OptionsStruct
 
-var thread_regex *regexp.Regexp
-var fn_regex *regexp.Regexp
-var src_regex *regexp.Regexp
-var rng *math_rand.Rand
+var rng *mathrand.Rand
 
 type reportPayload struct {
 	stack       []byte
@@ -55,21 +52,9 @@ var block_chan = make(chan struct{})
 
 func init() {
 	var err error
-	thread_regex, err = regexp.Compile(`^(.*):$`)
-	if err != nil {
-		panic(err)
-	}
-	fn_regex, err = regexp.Compile(`^(created by )?(.*)\.([^(]+)`)
-	if err != nil {
-		panic(err)
-	}
-	src_regex, err = regexp.Compile(`^\s*(.*):(\d+)`)
-	if err != nil {
-		panic(err)
-	}
 
 	var seed_bytes [8]byte
-	_, err = crypto_rand.Read(seed_bytes[:])
+	_, err = cryptorand.Read(seed_bytes[:])
 	if err != nil {
 		panic(err)
 	}
@@ -84,74 +69,10 @@ func init() {
 			(int64(seed_bytes[6]) << 6) |
 			(int64(seed_bytes[7]) << 7)
 
-	rand_source := math_rand.NewSource(seed)
-	rng = math_rand.New(rand_source)
+	rand_source := mathrand.NewSource(seed)
+	rng = mathrand.New(rand_source)
 
 	go sendWorkerMain()
-}
-
-type thread struct {
-	id     int
-	name   string
-	frames []frame
-}
-
-type frame struct {
-	fn_name     string
-	library     string
-	source_path string
-	line        int
-}
-
-func getNextLine(lines [][]byte, index *int) []byte {
-	if *index < len(lines) && len(lines[*index]) != 0 {
-		result := lines[*index]
-		*index += 1
-		return result
-	}
-	*index += 1
-	return nil
-}
-
-func getThread(lines [][]byte, index *int) *thread {
-	thread_line := getNextLine(lines, index)
-	if thread_line == nil {
-		return nil
-	}
-	thread_matches := thread_regex.FindSubmatch(thread_line)
-	if thread_matches == nil {
-		return nil
-	}
-	thread_item := thread{
-		id:   *index,
-		name: string(thread_matches[1]),
-	}
-	for {
-		fn_line := getNextLine(lines, index)
-		if fn_line == nil {
-			break
-		}
-		src_line := getNextLine(lines, index)
-		if src_line == nil {
-			break
-		}
-
-		fn_name_matches := fn_regex.FindSubmatch(fn_line)
-		source_path_matches := src_regex.FindSubmatch(src_line)
-
-		new_frame := frame{}
-		if fn_name_matches != nil {
-			new_frame.library = string(fn_name_matches[2])
-			new_frame.fn_name = string(fn_name_matches[3])
-		}
-		if source_path_matches != nil {
-			new_frame.source_path = string(source_path_matches[1])
-			new_frame.line, _ = strconv.Atoi(string(source_path_matches[2]))
-		}
-
-		thread_item.frames = append(thread_item.frames, new_frame)
-	}
-	return &thread_item
 }
 
 func Report(object interface{}, extra_attributes map[string]interface{}) {
@@ -275,32 +196,6 @@ func checkOptions() bool {
 	return true
 }
 
-func collectSource(source_path string, source_path_to_id map[string]string, source_code_json map[string]interface{}, next_source_id *int) string {
-	existing_id, present := source_path_to_id[source_path]
-	if present {
-		return existing_id
-	}
-	new_id := strconv.Itoa(*next_source_id)
-	*next_source_id += 1
-	source_path_to_id[source_path] = new_id
-
-	source_code_object := map[string]interface{}{}
-
-	bytes, err := ioutil.ReadFile(source_path)
-	if err == nil {
-		source_code_object["text"] = string(bytes)
-		source_code_object["startLine"] = 1
-		source_code_object["startColumn"] = 1
-		source_code_object["startPos"] = 0
-		source_code_object["tabWidth"] = Options.TabWidth
-	}
-	source_code_object["path"] = source_path
-
-	source_code_json[new_id] = source_code_object
-
-	return new_id
-}
-
 func createUuid() string {
 	var uuid_bytes [16]byte
 	_, _ = rng.Read(uuid_bytes[:]) // This function is documented to never fail.
@@ -345,12 +240,7 @@ func finishSendingReports(kill bool) {
 }
 
 func processAndSend(payload *reportPayload) {
-	lines := bytes.Split(payload.stack, []byte{'\n'})
-
-	source_path_to_id := map[string]string{}
-	source_code := map[string]interface{}{}
-
-	threads := map[string]interface{}{}
+	threads, mainThread, sourceCode := ParseThreadsFromStack(payload.stack)
 
 	report := map[string]interface{}{}
 	report["uuid"] = createUuid()
@@ -362,39 +252,9 @@ func processAndSend(payload *reportPayload) {
 	report["attributes"] = payload.attributes
 	report["annotations"] = payload.annotations
 	report["threads"] = threads
-	report["sourceCode"] = source_code
+	report["mainThread"] = mainThread
+	report["sourceCode"] = sourceCode
 	report["classifiers"] = []string{payload.classifier}
-
-	next_source_id := 0
-	line_index := 0
-	first := true
-	for line_index < len(lines) {
-		thread_item := getThread(lines, &line_index)
-		if thread_item == nil {
-			break
-		}
-		thread_id := strconv.Itoa(thread_item.id)
-		if first {
-			first = false
-			report["mainThread"] = thread_id
-		}
-		stack_list := []interface{}{}
-		for _, frame_item := range thread_item.frames {
-			source_code_id := collectSource(frame_item.source_path, source_path_to_id, source_code, &next_source_id)
-
-			stack_frame := map[string]interface{}{}
-			stack_frame["funcName"] = frame_item.fn_name
-			stack_frame["library"] = frame_item.library
-			stack_frame["sourceCode"] = source_code_id
-			stack_frame["line"] = frame_item.line
-			stack_list = append(stack_list, stack_frame)
-		}
-
-		thread_map := map[string]interface{}{}
-		threads[thread_id] = thread_map
-		thread_map["name"] = thread_item.name
-		thread_map["stack"] = stack_list
-	}
 
 	full_url := fmt.Sprintf("%s/post?format=json&token=%s", Options.Endpoint, url.QueryEscape(Options.Token))
 	if Options.DebugBacktrace {
@@ -407,7 +267,6 @@ func processAndSend(payload *reportPayload) {
 		fmt.Fprintf(os.Stderr, "%s\n", string(json_bytes))
 	}
 
-	var err error
 	json_bytes, err := json.Marshal(report)
 	if err != nil {
 		if Options.DebugBacktrace {
---
+++ threads_test.go
@@ -0,0 +1,123 @@
+package bt
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+const stackExample1 = `goroutine 1 [running]:
+main.GetStack()
+	/tmp/sandbox889685435/prog.go:30 +0x5f
+main.main()
+	/tmp/sandbox889685435/prog.go:15 +0x2f
+
+goroutine 6 [runnable]:
+main.testFunc()
+	/tmp/sandbox889685435/prog.go:22
+created by main.main in goroutine 1
+	/tmp/sandbox889685435/prog.go:13 +0x1e
+
+goroutine 7 [runnable]:
+main.testFunc()
+	/tmp/sandbox889685435/prog.go:22
+created by main.main in goroutine 1
+	/tmp/sandbox889685435/prog.go:14 +0x2a
+`
+
+func TestParseThreadsFromStack(t *testing.T) {
+	t.Run("example1_ok", func(t *testing.T) {
+		expectedThreads := map[string]interface{}{
+			"1": map[string]interface{}{
+				"name": "goroutine 1 [running]",
+				"stack": []map[string]interface{}{
+					{
+						"funcName":   "GetStack",
+						"library":    "main",
+						"line":       30,
+						"sourceCode": "0",
+					},
+					{
+						"funcName":   "main",
+						"library":    "main",
+						"line":       15,
+						"sourceCode": "0",
+					},
+				},
+			},
+
+			"7": map[string]interface{}{
+				"name": "goroutine 6 [runnable]",
+				"stack": []map[string]interface{}{
+					{
+						"funcName":   "testFunc",
+						"library":    "main",
+						"line":       22,
+						"sourceCode": "0",
+					},
+					{
+						"funcName":   "main",
+						"library":    "main",
+						"line":       13,
+						"sourceCode": "0",
+					},
+				},
+			},
+
+			"13": map[string]interface{}{
+				"name": "goroutine 7 [runnable]",
+				"stack": []map[string]interface{}{
+					{
+						"funcName":   "testFunc",
+						"library":    "main",
+						"line":       22,
+						"sourceCode": "0",
+					},
+					{
+						"funcName":   "main",
+						"library":    "main",
+						"line":       14,
+						"sourceCode": "0",
+					},
+				},
+			},
+		}
+		expectedMainThread := "1"
+		expectedSourceCode := map[string]interface{}{
+			"0": map[string]interface{}{
+				"path": "/tmp/sandbox889685435/prog.go",
+			},
+		}
+
+		threads, mainThread, sourceCode := ParseThreadsFromStack([]byte(stackExample1))
+		requireJSONEqual(t, expectedThreads, threads)
+		requireEqual(t, expectedMainThread, mainThread)
+		requireEqual(t, expectedSourceCode, sourceCode)
+	})
+}
+
+func requireJSONEqual(t *testing.T, expectedVal interface{}, val interface{}) {
+	expectedValJSON, err := json.Marshal(expectedVal)
+	requireNoErr(t, err)
+	valJSON, err := json.Marshal(val)
+	requireNoErr(t, err)
+
+	expectedValJSONStr := string(expectedValJSON)
+	valJSONStr := string(valJSON)
+
+	if expectedValJSONStr != valJSONStr {
+		t.Fatalf("unexpected JSON inequality; following values are not equal:\n%v\n%v", expectedValJSONStr, valJSONStr)
+	}
+}
+
+func requireEqual(t *testing.T, expectedVal interface{}, val interface{}) {
+	if !reflect.DeepEqual(expectedVal, val) {
+		t.Fatalf("unexpected inequality; following values are not equal:\n%v (%T)\n%v (%T)", expectedVal, expectedVal, val, val)
+	}
+}
+
+func requireNoErr(t *testing.T, err error) {
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
